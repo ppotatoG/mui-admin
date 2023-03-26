8da825393b81ab62d9539b2e133989d5d23f5f5e
ppotatoG <thgus2270@gmail.com>
2023-03-26 23:59:06 +0900

    Feat: MUI theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@styles/globals.scss';
+import '@styles/_themes-vars.module.scss';
 import type { AppProps } from 'next/app';
 
 import i18n from 'i18next';
@@ -6,6 +6,9 @@ import { initReactI18next, I18nextProvider } from 'react-i18next';
 
 import enTranslation from '@public/locales/en/translation.json';
 import koTranslation from '@public/locales/ko/translation.json';
+
+import { theme } from '@themes';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 i18n.use(initReactI18next).init({
   lng: 'en',
@@ -27,7 +30,9 @@ i18n.use(initReactI18next).init({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <I18nextProvider i18n={i18n}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </I18nextProvider>
   );
 }
