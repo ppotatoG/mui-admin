d1d9d879fc8bcb2db63c6c073fef2561b344221b
ppotatoG <thgus2270@gmail.com>
2023-03-29 08:50:47 +0900

    Revert "Feat: MUI theme"
    This reverts commit 8da825393b81ab62d9539b2e133989d5d23f5f5e.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@styles/_themes-vars.module.scss';
+import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 import i18n from 'i18next';
@@ -6,9 +6,6 @@ import { initReactI18next, I18nextProvider } from 'react-i18next';
 
 import enTranslation from '@public/locales/en/translation.json';
 import koTranslation from '@public/locales/ko/translation.json';
-
-import { theme } from '@themes';
-import { ThemeProvider } from '@material-ui/core/styles';
 
 i18n.use(initReactI18next).init({
   lng: 'en',
@@ -30,9 +27,7 @@ i18n.use(initReactI18next).init({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <I18nextProvider i18n={i18n}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </I18nextProvider>
   );
 }
