f8625e6f62ea8daf7687601aa2686ff6c8162108
ppotatoG <thgus2270@gmail.com>
2023-03-31 11:18:30 +0900

    Configure: styled-components for Next.js.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+
+  webpack(conf) {
+    conf.resolve.modules.push(__dirname);
+    return conf;
+  },
+});
