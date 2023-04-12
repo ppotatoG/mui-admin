229ec65bc6076e057166aff9c48ddf75123bc388
ppotatoG <thgus2270@gmail.com>
2023-04-12 15:14:27 +0900

    Feat: Navigation

--- layout/Navigate/index.tsx
+++ layout/Navigate/index.tsx
@@ -1,5 +1,30 @@
+import styled from 'styled-components';
+import { useContext } from 'react';
+import themes from '@styles/themes';
+import { ThemeContext } from '@contexts/ThemeContext';
+
 const Navigate = () => {
-  return <div>Navigate</div>;
+  return (
+    <Nav>
+      <ul>
+        <li>menu1</li>
+        <li>menu2</li>
+        <li>menu3</li>
+        <li>menu4</li>
+        <li>menu5</li>
+        <li>menu6</li>
+      </ul>
+    </Nav>
+  );
 };
+
+const Nav = styled.nav`
+  background-color: ${() => {
+    const { isDarkMode } = useContext(ThemeContext);
+    return isDarkMode ? themes.dark.paper : themes.light.primary;
+  }};
+  color: ${() => themes.light.paper};
+  padding: ${() => themes.fontSizes.medium};
+`;
 
 export default Navigate;
